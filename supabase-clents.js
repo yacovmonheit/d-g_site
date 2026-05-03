d40362d75a67ad123dfef75ac881a138458d9e4b
yacovmonheit <z0491285303@gmail.com>
2026-05-03 14:27:12 +0200

    Add files via upload

---
+++ supabase-clents.js
@@ -0,0 +1,26 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.https://xudussyohxdprkdahcsn.supabase.co;
+const supabaseKey = import.meta.env.sb_publishable_YknHTm-o-5ShcJ5rNzH2hQ_GjSUsP7X;
+
+const supabase = createClient('https://xudussyohxdprkdahcsn.supabase.co/rest/v1/', 'sb_publishable_YknHTm-o-5ShcJ5rNzH2hQ_GjSUsP7X');
+
+export default supabase;
+
+console.log(import.meta.env.https://xudussyohxdprkdahcsn.supabase.co);
+
+
+
+
+
+git add .
+git commit -m "first commit"
+git push origin main
+
+echo "# pop" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yacovmonheit/pop.git
+git push -u origin main
